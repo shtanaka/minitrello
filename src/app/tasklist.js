ae86a1b893ba17c20856ac88fc9679967025d471
Edson Gustavo Santiago Silva <egustavo4@gmail.com>
2017-07-26 08:54:59 -0300

    remove js and map

--- src/app/tasklist.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var TaskList = (function () {
-    function TaskList() {
-        this.tasks = [];
-    }
-    return TaskList;
-}());
-exports.TaskList = TaskList;
-//# sourceMappingURL=tasklist.js.map
